3fc41ca4246ac57cb339883994c68669ea3ea340
carcarvroom <carly_l@outlook.com>
2020-08-31 20:54:15 -0700

    successfully added css theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom"
+
+import AdminLayout from './components/layouts/Admin'
+import AuthLayout from './components/layouts/Auth'
+
 import Sidebar from './components/navbars/Sidebar'
 import TaskBoard from './boards/taskBoard/TaskBoard'
-import routes from "./routes.js";
+import routes from "./routes.js"
 
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div>
+      {/* <div>
         <Sidebar routes={routes}
         logo={{
             innerLink: "/admin/index",
@@ -18,7 +22,13 @@ class App extends Component {
           }} />
         <h1>This is app</h1>
         <TaskBoard />
-      </div>
+      </div> */}
+        <Switch>
+          <Route path="/admin" render={props => <AdminLayout {...props} />} />
+          <Route path="/auth" render={props => <AuthLayout {...props} />} />
+          {/* redirect to /simplize */}
+          <Redirect from="/" to="/auth" />
+        </Switch>
       </BrowserRouter>
     )
   }
---
+++ src/components/footers/AuthFooter.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+// reactstrap components
+import { NavItem, NavLink, Nav, Container, Row, Col } from "reactstrap";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <>
+        <footer className="py-5">
+          <Container>
+            <Row className="align-items-center justify-content-xl-between">
+              <Col xl="6">
+                <div className="copyright text-center text-xl-left text-muted">
+                  © 2018{" "}
+                  <a
+                    className="font-weight-bold ml-1"
+                    href="https://www.linkedin.com/in/carlyla/"
+                    target="_blank"
+                  >
+                    Creative Tim
+                  </a>
+                </div>
+              </Col>
+              <Col xl="6">
+                <Nav className="nav-footer justify-content-center justify-content-xl-end">
+                  <NavItem>
+                    <NavLink
+                      href="https://medium.com/@carly_l"
+                      target="_blank"
+                    >
+                      Blog
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      href="https://github.com/carcarvroom/project-frontend"
+                      target="_blank"
+                    >
+                      Github
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </Col>
+            </Row>
+          </Container>
+        </footer>
+      </>
+    );
+  }
+}
+
+export default Login;
